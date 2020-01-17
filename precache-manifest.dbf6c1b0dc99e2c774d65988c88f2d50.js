c6cdb476c2154f2081a0565082d326a86db40823
Isaac Adamu <isaacmagaji1@gmail.com>
2020-01-17 11:38:34 +0100

    Updates

---
+++ precache-manifest.dbf6c1b0dc99e2c774d65988c88f2d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ff6d8cc53b8fbdd3b4ea3ef065aabe",
+    "url": "/contactOrganizer/index.html"
+  },
+  {
+    "revision": "ce00f8ce9eed21c14208",
+    "url": "/contactOrganizer/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7146b1a1cd521557aac0",
+    "url": "/contactOrganizer/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ce00f8ce9eed21c14208",
+    "url": "/contactOrganizer/static/js/2.c7ac32a4.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactOrganizer/static/js/2.c7ac32a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7146b1a1cd521557aac0",
+    "url": "/contactOrganizer/static/js/main.02082eab.chunk.js"
+  },
+  {
+    "revision": "490584296b264a4a1146",
+    "url": "/contactOrganizer/static/js/runtime-main.05c2ed05.js"
+  }
+]);
